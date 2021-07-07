06e3578c53145ca93b56fd26e71270fb4fbf53b4
brunogm2 <brunobgm002@gmail.com>
2021-07-07 01:03:50 -0300

    adding surveys and implementing tests

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-import { User } from "../models/User";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
-class UseController{
+class UserController{
     async create(req: Request, res: Response){
         const {name, email} = req.body;
 
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
 
         const userAlreadyExists = await usersRepository.findOne({
             email
@@ -24,8 +24,9 @@ class UseController{
 
         await usersRepository.save(user);
 
-        return res.json(user);
+        return res.status(201).json(user);
     }
 }
 
-export { UseController };
+export { UserController };
+
